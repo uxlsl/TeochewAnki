65f76c992a9505689c2efd55e2fe5eade1f9bc33
lin <lin@lindeMacBook-Pro.local>
2021-12-05 11:32:20 +0800

    fix

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 import os
 import requests
 from pyquery import PyQuery as pq
+import genanki
 
 
-MP3_PATH = "mp3"
+MP3_PATH = "."
+my_deck = genanki.Deck(
+  2059400110,
+  '潮汕话日常宝典')
+
+my_model = genanki.Model(
+  1091735104,
+  'Simple Model with Media',
+  fields=[
+    {'name': 'Question'},
+    {'name': 'Answer'},
+    {'name': 'MyMedia1'},                                 
+    {'name': 'MyMedia2'},                                 
+  ],
+  templates=[
+    {
+      'name': 'Card 1',
+      'qfmt': '{{Question}}<br>{{MyMedia1}} <br> {{MyMedia2}}',
+      'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+    },
+  ])
+  
+my_package = genanki.Package(my_deck)
 
 
 def getWords():
@@ -21,26 +44,36 @@ def merge(word, paths):
     cmd = 'cd {} && test -f {} || ffmpeg -i "concat:{}" -acodec copy {}'\
         .format(MP3_PATH, outpath, '|'.join([os.path.basename(p) for p in paths]), outpath)
     os.system(cmd)
-    return os.path.join(MP3_PATH, outpath)
+    #return os.path.join(MP3_PATH, outpath)
+    return outpath
 
 def check(word):
     """
     返回多个mp3保存地址
     """
     print(word)
-    url = "http://www.czyzd.com/search?keyword={}"
-    r = requests.get(url.format(word))
-    d = pq(r.text)
     ret = []
+    req = None
+    find_one = False
     for i in [1,2]:
-        path = '{}/{}{}.mp3'.format(MP3_PATH, word, i)
+        path = '{}{}.mp3'.format(word, i)
         if not os.path.exists(path):
+            print(path)
+            if find_one:
+                continue
+            url = "http://www.czyzd.com/search?keyword={}"
+            if req is None:
+                req = requests.get(url.format(word))
+            d = pq(req.text)
             sel = '#list > dl > dd > ul > li:nth-child({}) > button'.format(i)
             a = d(sel)
             if a.attr['data-rel']:
                 r = requests.get(a.attr['data-rel'])
                 open(path, 'wb').write(r.content)
                 ret.append(path)
+        else:
+            find_one = True
+            ret.append(path)
     return ret
 
 def main():
@@ -66,6 +99,23 @@ def main():
             else:
                 mem[word['word']].append(
                     merge(word['word']+str(i), paths))
+        if mem[word['word']]:
+            voicePath1 = mem[word['word']][0]
+            if len(mem[word['word']]) == 2:
+                voicePath2 = mem[word['word']][1]
+            else:
+                voicePath2 = ""
+            my_note = genanki.Note(model=my_model,
+                fields=[word['word'], word['mean'], 
+                '[sound:{}]'.format(voicePath1),
+                '[sound:{}]'.format(voicePath2),
+                ])
+            my_deck.add_note(my_note)
+            for voicePath in mem[word['word']]:
+                my_package.media_files.append(voicePath)
+    
+    my_package.write_to_file('Teachew.apkg')
+
 
 if __name__ == '__main__':
     main()
