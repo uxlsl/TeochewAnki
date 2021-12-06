c0bc4234b5492eaee90e1c76cdc9c2549e03b083
linsonglin <linsonglin@roboeye.ai>
2021-12-07 07:31:46 +0800

    v1

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ my_model = genanki.Model(
   fields=[
     {'name': 'Question'},
     {'name': 'Answer'},
-    {'name': 'MyMedia1'},                                 
-    {'name': 'MyMedia2'},                                 
+    {'name': 'MyMedia1'},
+    {'name': 'MyMedia2'},
   ],
   templates=[
     {
@@ -25,7 +25,7 @@ my_model = genanki.Model(
       'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
     },
   ])
-  
+
 my_package = genanki.Package(my_deck)
 
 
@@ -37,7 +37,7 @@ def getWords():
             info['word'] = tmp[0]
             if len(tmp) > 2:
                 info['mean'] = tmp[-1]
-            yield info 
+            yield info
 
 def merge(word, paths):
     outpath = '{}.mp3'.format(word)
@@ -52,29 +52,24 @@ def check(word):
     返回多个mp3保存地址
     """
     print(word)
-    ret = []
     req = None
-    find_one = False
+    mp3paths = []
+    url = "http://www.czyzd.com/search?keyword={}"
+    req = requests.get(url.format(word))
+    d = pq(req.text)
+    mean = ""
     for i in [1,2]:
-        path = '{}{}.mp3'.format(word, i)
-        if not os.path.exists(path):
-            print(path)
-            if find_one:
-                continue
-            url = "http://www.czyzd.com/search?keyword={}"
-            if req is None:
-                req = requests.get(url.format(word))
-            d = pq(req.text)
-            sel = '#list > dl > dd > ul > li:nth-child({}) > button'.format(i)
-            a = d(sel)
-            if a.attr['data-rel']:
-                r = requests.get(a.attr['data-rel'])
-                open(path, 'wb').write(r.content)
-                ret.append(path)
-        else:
-            find_one = True
-            ret.append(path)
-    return ret
+        sel = '#list > dl > dd > ul > li:nth-child({})'.format(i)
+        mean += " " + d(sel).text()
+        sel = '#list > dl > dd > ul > li:nth-child({}) > button'.format(i)
+        a = d(sel)
+        if a.attr['data-rel']:
+            r = requests.get(a.attr['data-rel'])
+            path = '{}{}.mp3'.format(word, i)
+            open(path, 'wb').write(r.content)
+            mp3paths.append(path)
+    return [mean, mp3paths]
+
 
 def main():
     os.makedirs(MP3_PATH, exist_ok=True)
@@ -86,34 +81,39 @@ def main():
                     mem[w] = check(w)
                     print(mem[w])
                 except Exception as e:
-                    pass
+                    print(e)
         if len(word['word']) == 1:
             continue
-        mem[word['word']] = []
+        mem[word['word']] = ["", []]
         for i in [0,1]:
             paths = []
+            mean = ""
             for w in word['word']:
-                if i > len(mem[w]) - 1:
+                if i > len(mem[w][1]) - 1:
                     break
-                paths.append(mem[w][i])
+                paths.append(mem[w][1][i])
+                mean += mem[w][0]
             else:
-                mem[word['word']].append(
+                mem[word['word']][1].append(
                     merge(word['word']+str(i), paths))
-        if mem[word['word']]:
-            voicePath1 = mem[word['word']][0]
-            if len(mem[word['word']]) == 2:
-                voicePath2 = mem[word['word']][1]
+                mem[word['word']][0] = mean
+
+        if mem[word['word']][1]:
+            print(mem[word['word']])
+            voicePath1 = mem[word['word']][1][0]
+            if len(mem[word['word']][1]) == 2:
+                voicePath2 = mem[word['word']][1][1]
             else:
                 voicePath2 = ""
             my_note = genanki.Note(model=my_model,
-                fields=[word['word'], word['mean'], 
+                fields=[word['word'], mem[word['word']][0],
                 '[sound:{}]'.format(voicePath1),
                 '[sound:{}]'.format(voicePath2),
                 ])
             my_deck.add_note(my_note)
-            for voicePath in mem[word['word']]:
+            for voicePath in mem[word['word']][1]:
                 my_package.media_files.append(voicePath)
-    
+
     my_package.write_to_file('Teachew.apkg')
 
 
